f9944430e65fc7d4056d38be74f1e3be6a8c2e60
theodaguier <theo.daguier@icloud.com>
2023-10-27 15:41:36 +0200

    update

--- expo-sf-symbols/index.tsx
+++ expo-sf-symbols/index.tsx
@@ -11,7 +11,7 @@ interface Props {
   opacity?: number;
 }
 
-export function SFSymbols({
+function SFSymbols({
   name,
   width = 24,
   height = 24,
@@ -24,3 +24,5 @@ export function SFSymbols({
     <IconComponent width={width} height={height} opacity={opacity} {...props} />
   );
 }
+
+export default SFSymbols;
