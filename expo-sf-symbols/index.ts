1c1d7a532b88f9ef077f3d3db358db2f7da07610
theodaguier <theo.daguier@icloud.com>
2023-10-30 10:08:49 +0100

    updt

--- expo-sf-symbols/index.ts
+++ expo-sf-symbols/index.ts
@@ -2,7 +2,7 @@
 
 import * as SFSymbols from "./src/";
 
-module.exports = SFSymbols;
+export default SFSymbols;
 
 // Path: expo-sf-symbols/src/index.ts
 // index.ts main file for expo-sf-symbols/src
